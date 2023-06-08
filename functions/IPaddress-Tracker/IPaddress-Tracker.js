68494ccea9bae40bdfa9ca809be0539043399bec
Pooja <87766018+design005@users.noreply.github.com>
2023-06-08 11:47:49 +0000

    inital commit

---
+++ functions/IPaddress-Tracker/IPaddress-Tracker.js
@@ -0,0 +1,31 @@
+// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+
+exports.handler = async function (event) {
+   
+  let ipAddress = event.queryStringParameters.ipAddress;
+  // if (!ipAddress) {
+  //   // If no specific IP address is provided, get the user's IP address from the request headers
+  //   ipAddress = event.headers["true-client-iP"];
+  //  console.log(ipAddress);
+  //  
+  // }
+  const API_KEY = process.env.API_KEY   
+       const Url=`https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${ipAddress}`
+    
+ try{
+  const response = await fetch(Url);
+  const data = await response.json();
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data),
+   }
+}
+  catch(error){
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error),
+    }
+
+ }
+  
+}
---
+++ src/script.js
@@ -0,0 +1,59 @@
+
+const ip_Input=document.querySelector("#search");
+const form=document.querySelector("form");
+const IP_address=document.querySelector("#Ip-address");
+const location_IP=document.querySelector("#location");
+const time_Zone= document.querySelector("#time-zone");
+const ISP = document.querySelector("#isp");
+const map = L.map('map').setView([0, 0], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+window.addEventListener("load", async function (event) {
+    event.preventDefault();
+        const response = await fetch('https://api.ipify.org?format=json');
+      const data = await response.json();
+          const userIpAddress = data.ip;
+      ip_Input.value = userIpAddress; // Set the user's own IP address in the input field
+      getIpinfo(userIpAddress);
+   
+  });
+form.addEventListener("submit",function (event){
+   event.preventDefault();
+   ipValue = ip_Input.value;
+ getIpinfo(ipValue);
+  });
+
+
+async function getIpinfo(ip) {
+  try {
+    const response = await fetch(`/.netlify/functions/IPaddress-Tracker?ipAddress=${ip}`);
+    const data = await response.json();
+    renderInfo(data);
+    } catch (error) {
+      renderError();
+  }
+}
+
+function renderInfo(data){
+    IP_address.innerHTML=data.ip;
+  location_IP.innerHTML = `${data.location.city},${data.location.country}  ${data.location.postalCode}` ;
+   time_Zone.innerHTML=  ` UTC${data.location.timezone}`;
+ ISP.innerHTML=data.isp;
+  const {location:{lat, lng} } = data;
+  map.setView([lat, lng], 13);
+  L.marker([lat, lng]).addTo(map);
+   }
+
+function renderError(){
+  alert("Whoops, something went wrong. Please try again later!");
+  IP_address.innerHTML=" ";
+  location_IP.innerHTML = " ";
+   time_Zone.innerHTML= " ";
+ ISP.innerHTML=" ";
+ map.setView([0, 0], 13);
+
+}
